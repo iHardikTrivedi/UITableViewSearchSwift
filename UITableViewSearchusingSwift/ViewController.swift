2b59bbda09fe9a4b84df498d2714e9e6f1541b3e
Hardik M. Trivedi <ihart.iosdeveloper@gmail.com>
2015-10-14 20:31:47 +0530

    Created : 14-10-2015

---
+++ UITableViewSearchusingSwift/ViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  UITableViewSearchusingSwift
+//
+//  Created by Hardik Trivedi on 14/10/2015.
+//  Copyright (c) 2015 iHartDevelopers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController, UISearchDisplayDelegate, UISearchBarDelegate
+{
+    var marrCountryList = [String]()
+    var marrFilteredCountryList = [String]()
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        marrCountryList = ["USA", "Bahamas", "Brazil", "Canada", "Republic of China", "Cuba", "Egypt", "Fiji", "France", "Germany", "Iceland", "India", "Indonesia", "Jamaica", "Kenya", "Madagascar", "Mexico", "Nepal", "Oman", "Pakistan", "Poland", "Singapore", "Somalia", "Switzerland", "Turkey", "UAE", "Vatican City"]
+        
+        self.tableView.reloadData()
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        if tableView == self.searchDisplayController!.searchResultsTableView {
+            return self.marrFilteredCountryList.count
+        } else {
+            return self.marrCountryList.count
+        }
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let cellCountry = self.tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+        
+        var countryName : String!
+        if tableView == self.searchDisplayController!.searchResultsTableView {
+            countryName = marrFilteredCountryList[indexPath.row]
+        } else {
+            countryName = marrCountryList[indexPath.row]
+        }
+        
+        cellCountry.textLabel?!.text = countryName
+        
+        return cellCountry as! UITableViewCell
+    }
+    
+    func filterTableViewForEnterText(searchText: String) {
+        self.marrFilteredCountryList = self.marrCountryList.filter({( strCountry : String) -> Bool in
+            let stringForSearch = strCountry.rangeOfString(searchText)
+            return (stringForSearch != nil)
+        })
+    }
+    
+    func searchDisplayController(controller: UISearchDisplayController, shouldReloadTableForSearchString searchString: String?) -> Bool {
+        self.filterTableViewForEnterText(searchString!)
+        return true
+    }
+    
+    func searchDisplayController(controller: UISearchDisplayController,
+        shouldReloadTableForSearchScope searchOption: Int) -> Bool {
+            self.filterTableViewForEnterText(self.searchDisplayController!.searchBar.text!)
+            return true
+    }
+    
+}
+
